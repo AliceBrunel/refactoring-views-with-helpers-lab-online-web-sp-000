31cccdcb8c56720983e109d63b51738aa975db7b
Alice Brunel <alc.brunel@gmail.com>
2019-11-03 14:23:55 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-
+    @artist_name
   end
 
   def artist_name=(name)
-
+    @artist_name = Artist.find_by(name: name)
   end
 end
