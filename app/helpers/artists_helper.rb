f5f3a18b847607069f04dd8b869b3b9e7eae2378
Alice Brunel <alc.brunel@gmail.com>
2019-11-03 14:31:31 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,7 +2,7 @@ module ArtistsHelper
 
   def display_artist(song)
     # Using a parameter because we don't know how song is specified
-    if song.artist 
+    if song.artist
       link_to song.artist.name, song.artist
     else
       link_to 'add Artist', song.edit
